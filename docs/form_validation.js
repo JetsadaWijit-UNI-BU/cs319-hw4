ea65c57a27c83897ef95d4f20183dfc905fe91e3
JetsadaWijit <jetsadawijit@outlook.com>
2026-09-13 13:24:15 +0000

    docs: add space

--- docs/form_validation.js
+++ docs/form_validation.js
@@ -47,7 +47,7 @@ function username_validation(uname, min, max) {
   let uname_len = uname.value.length;
   if (uname_len == 0 || uname_len >= max || uname_len < min) {
     alert(
-      "Username should not be empty / length be between " + min + "to " + max,
+      "Username should not be empty / length be between " + min + " to " + max,
     );
     uname.focus();
     return false;
