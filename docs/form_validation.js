6875fd2b33160aed642a50da4c7e2460cb49f737
JetsadaWijit <jetsadawijit@outlook.com>
2026-09-13 12:26:22 +0000

    chore: create simple value

--- docs/form_validation.js
+++ docs/form_validation.js
@@ -6,4 +6,13 @@ function handleFormSubmit() {
   return false;
 }
 
-function formValidation() {}
+function formValidation() {
+  let uname = document.registration.uname;
+  let upass = document.registration.upass;
+  let ufname = document.registration.fname;
+  let ulname = document.registration.uname;
+  let uemail = document.registration.email;
+  let uadd = document.registration.address; /* User address */
+  let ucountry = document.registration.country;
+  let uzip = document.registration.zip;
+}
