6a72f4c83a3731e9f73268d2436e8c0d2bddc3e5
JetsadaWijit <jetsadawijit@outlook.com>
2026-09-13 12:41:06 +0000

    feat: add check for first and last name

--- docs/form_validation.js
+++ docs/form_validation.js
@@ -16,8 +16,14 @@ function formValidation() {
   let ucountry = document.registration.country;
   let uzip = document.registration.zip;
 
-  if (username_validation(uname, 5, 12)) {
+  if (username_validation(uname, 5, 12) && allLetter(uname)) {
     if (pass_validation(upass, 6, 16)) {
+      if (allLetter(ufname)) {
+        /* Check first name */
+        if (allLetter(ulname)) {
+          /* Check last name */
+        }
+      }
     }
   }
   return false;
